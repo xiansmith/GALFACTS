ded05474ddb2cd1cdbe1a21024d355082f2569f5
jeff <jeff>
2007-02-09 19:36:39 +0000

    minor update

--- src/stokes.h
+++ src/stokes.h
@@ -5,7 +5,6 @@
 #define _STOKES_H
 
 void calculate_stokes(SpecRecord dataset[], int size, int lowchan, int highchan, int ignoreRFI, float Tcalx[], float Tcaly[]);
-//void smooth_stokes(SpecRecord dataset[], int size, int lowchan, int highchan, int tsmooth);
 void write_channel_data(SpecRecord dataset[], int size, int lowchan, int highchan);
 void average_stokes(SpecRecord dataset[], int size, int lowchan, int highchan);
 void correct_beamgains(SpecRecord dataset[], int size, int lowchan, int highchan, int beam);

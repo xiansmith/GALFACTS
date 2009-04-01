ef412979d84a013ab0928f3aceab9fd1f739732d
sguram <sguram>
2009-04-01 01:08:22 +0000

    MINOR: changes MAX_CHANNELS to 2048 and MAX_NUM_SCANS to 30

--- src/common.h
+++ src/common.h
@@ -5,7 +5,7 @@
 
 //The maximum number of channels in the dataset
 //#define MAX_CHANNELS 256
-#define MAX_CHANNELS 4096
+#define MAX_CHANNELS 2048
 
 //The maximum number of datapoints in a single scan
 #define MAX_SCAN_LEN 4000
@@ -14,20 +14,20 @@
 #define MAX_NUM_DAYS 200
 
 //The maximum number of scans in one direction in a single day
-#define MAX_NUM_SCANS 16
+#define MAX_NUM_SCANS 30
 
 #define NUM_BEAMS 7 //SSG
 //enum STOKES {STOKES_I, STOKES_Q, STOKES_U, STOKES_V};
 #define MULTIBEAM 8 //ssg
 
 enum RFIFlags {	RFI_NONE =		0x0000,
-				RFI_CALOFF_XX =	0x0001, 
+				RFI_CALOFF_XX =	0x0001,
 				RFI_CALOFF_YY =	0x0002,
-				RFI_CALON_XX =	0x0004, 
+				RFI_CALON_XX =	0x0004,
 				RFI_CALON_YY =	0x0008,
-				RFI_CALOFF_XY =	0x0010, 
+				RFI_CALOFF_XY =	0x0010,
 				RFI_CALOFF_YX =	0x0020,
-				RFI_CALON_XY =	0x0040, 
+				RFI_CALON_XY =	0x0040,
 				RFI_CALON_YX =	0x0080,
 				RFI_OVERLIMIT = 0x0100,
 				RFI_SIGMA_EXCEEDED = 0x0200,

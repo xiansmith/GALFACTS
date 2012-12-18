9a31f31ebf44867c6576f9f15ebc9e82350bdb0f
Christian Smith <csmith@vole.(none)>
2012-12-18 14:43:04 -0700

    Fixed header values for all headers, and average image calculations

--- map/map.c
+++ map/map.c
@@ -71,22 +71,98 @@ static void create_header_param_list(header_param_list * hpar_ptr, MapMetaData *
 	sprintf (hpar_ptr->ctype[1], "DEC--CAR");
 	sprintf (hpar_ptr->ctype[2], "FREQ");
 	hpar_ptr->crval[0] = md->RAcen;		/* degrees */
-	hpar_ptr->crval[1] = md->DECcen;    /* degrees */
-	hpar_ptr->crval[2] = md->fstart;  /* khz */ 
+	hpar_ptr->crval[1] = 0.0; // SSG correction    md->DECcen;    /* degrees */
+	hpar_ptr->crval[2] = md->fstart;  /* khz */   // for avg is favg (compute this = average of freq)
 	
 	hpar_ptr->crpix[0] = 0.5 + md->n1 / 2.0;	/* image center in pixels */
-	hpar_ptr->crpix[1] = 0.5 + md->n2 / 2.0;
-	hpar_ptr->crpix[2] = 1.0;
+	hpar_ptr->crpix[1] = 0.5 + md->n2 / 2.0 - (md->DECcen / md->cellsize); // SSG correction for fits standard
+	hpar_ptr->crpix[2] = 1.0; // fine for avg
 	hpar_ptr->cdelt[0] = -md->cellsize;                     /* degrees */
 	hpar_ptr->cdelt[1] = md->cellsize;                     /* degrees */
 	//hpar_ptr->cdelt[2] = -172.032*1000000/MAX_CHANNELS;	//TODO: magic numbers				/* Mhz */
-	hpar_ptr->cdelt[2] = md->df; 
+	hpar_ptr->cdelt[2] = md->df;
 	
 	hpar_ptr->equinox = 2000.0;		//year of coordinate system
 	//hpar_ptr->epoch = 2004.???;		//year observations were taken
 
 	sprintf (hpar_ptr->bunit, "Kelvin");
-	sprintf (hpar_ptr->telescope, "Arecibo");
+	sprintf (hpar_ptr->telescope, "Arecibo"); // added for VO compatibility
+	sprintf (hpar_ptr->observer, "GALFACTS");
+	sprintf (hpar_ptr->instrument, "ALFA");
+
+	// for average images
+	if ( (md->lowchan == 0) && (md->highchan == 1) )
+	{
+		FILE * BadChannelsFile; BadChannelsFile = fopen("BadChannels.list", "r");
+		int *badchannels = (int*)calloc(MAX_CHANNELS, sizeof(int));
+
+		int i = 0;
+		int chan1, chan2;
+		int chanCount = 0.0;
+		float freqsum = 0.0;
+		float fstart = md->fstart;
+		float avgfreq = 0.0;
+		int goodLowChan = 0;
+		int goodHighChan = 0;
+		int firstGoodChan = 1;
+
+		if(BadChannelsFile != NULL)
+		{
+		int chan1, chan2;
+		do
+			{
+			fscanf(BadChannelsFile, "%d %d\n", &chan1, &chan2);
+			printf("Excluded: chan1 = %d chan2 = %d\n", chan1, chan2);
+			for(i=chan1; i<=chan2; i++)
+				{
+				badchannels[i] = 1;
+				}
+			}
+		while(!feof(BadChannelsFile));
+		fclose(BadChannelsFile);
+
+		}
+
+		i =0;
+
+		for( i=md->avg_lowchan; i <= md->avg_highchan; i++ ) {
+			if( badchannels[i] != 1 )
+			{
+				// record the first good low chan in case of channel exceptions
+				if ( firstGoodChan == 1 ){
+					goodLowChan = i;
+					firstGoodChan = 0;
+				}
+				freqsum += fstart + (i * md->df);
+				chanCount++;
+			}
+		}
+
+		// find highest good channel for crval[2]
+		firstGoodChan = 1;
+		for( i=md->avg_highchan; i <= md->avg_lowchan; i-- )
+		{
+			if( badchannels[i] != 1 )
+			{
+				// record the first good low chan in case of channel exceptions
+				if ( firstGoodChan == 1 )
+				{
+					goodHighChan = i;
+					firstGoodChan = 0;
+				}
+			}
+		}
+
+
+		avgfreq = freqsum / chanCount;
+
+		hpar_ptr->crval[2] = avgfreq;
+		hpar_ptr->cdelt[2] = md->df * (md->avg_highchan - md->avg_lowchan);  // for avg md->df * md->n3
+
+		printf("XXX For average channel got chanCount=%d fstart=%f freqsum=%f avgfreq=%f goodLowChan=%d goodHighChan=%d\n", chanCount, fstart, freqsum, avgfreq, goodLowChan, goodHighChan );
+
+	}
+
 }
 
 void start_fits_cubes(const char * wapp, MapMetaData *md)

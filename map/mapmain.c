cd7ad2b67a4ac9401dd342d96031f7698c04e16f
Christian Smith <csmith@ras.ucalgary.ca>
2013-04-08 17:01:53 -0600

    turned off single file balance files, causes issues

--- map/mapmain.c
+++ map/mapmain.c
@@ -164,8 +164,8 @@ static void create_fits_cube(FluxWappData * wappdata, char * wapp, MapMetaData *
 			balance_data(wappdata, md->day_iter, md->scan_iter, md->balgain, md->balepsilon, md->bw_order);
 		
 			//printf("Writing basketweaved time series ...\n");
-			//fluxwappdata_writechan_binary(wappdata,chan);
-			fluxwappdata_writechan_binary_single(wappdata,chan);
+			fluxwappdata_writechan_binary(wappdata,chan);
+			//fluxwappdata_writechan_binary_single(wappdata,chan);
 
 			printf("Gridding ...\n"); 
 			grid_data(wappdata, md, dataI, dataQ, dataU, dataV, weight);

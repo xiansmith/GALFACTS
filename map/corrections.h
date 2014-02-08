ac45094a157af809eee13dc0dee8b6c2e296ed0a
Sukhpreet Guram <sguram@cougar.ras.ucalgary.ca>
2014-02-08 12:52:18 -0700

    .h for corresponding .c

---
+++ map/corrections.h
@@ -0,0 +1,9 @@
+#include "common.h"
+#include "fluxdata.h"
+#include "map.h"
+
+#ifndef _CORRECTIONS_H
+#define _CORRECTIONS_H
+
+void corrections(FluxWappData * wappdata, int chan, MapMetaData *md);
+#endif

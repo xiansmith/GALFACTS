3b1393c2439f38f5650594ceb5a9c123092e9df7
Sukhpreet Guram <ssguram@ucalgary.ca>
2014-10-22 11:09:50 +0200

    abscal coeffs read from file

--- map/abscal.c
+++ map/abscal.c
@@ -10,8 +10,10 @@ static void absolute_cal_day(FluxWappData * wappdata, int day, int beam, int cha
 	FILE *table;
 	char filename[64];
 	float spec_I[7][MAX_CHANNELS];
-	int c,i,b;
-/*	sprintf(filename,"Icorr.dat");
+	int i,b;
+	float c[3];
+        sprintf(filename,"%s.abscal.txt",md->field);
+
 	table = fopen(filename,"r");
 	if(table == NULL)
 	{
@@ -20,71 +22,29 @@ static void absolute_cal_day(FluxWappData * wappdata, int day, int beam, int cha
 	}
 	else
 	{
-		for(i = 0; i < MAX_CHANNELS; i++)
+		for(i = 0; i < 3; i++)
 		{
-			fscanf(table ,"%d",&c);
-			//printf("%d ",c);
-			for(b=0;b<7;b++)
-			{
-				fscanf(table ,"%f",&spec_I[b][i]);
-				//printf("%f ",spec_I[b][i]);
-			}
-			//printf("\n");
+			fscanf(table ,"%f",&c[i]);
 		}
 		fclose(table);
 
-		if(md->avg)
+
+		FluxDayData * daydata = &wappdata->daydata[day];
+		int R = daydata->numRecords;
+		int r;
+		for (r = 0; r < R; r++)
 		{
-			int j;
-			for(j = md->avg_lowchan;j < md->avg_highchan;j+=md->avg)
+			if (isfinite(daydata->records[r].stokes.I))
 			{
-				int k;
-				for(k = j+1;k<j+md->avg;k++)
-				{
-					for(b=0;b<7;b++)
-					{
-						spec_I[b][j]+=spec_I[b][k];
-					}
-				}
-				for(b=0;b<7;b++)
-				{
-					spec_I[b][j]/=md->avg;
-				}
+				float RA = daydata->records[r].RA;
+				//S1
+				//daydata->records[r].stokes.I -= (0.0000479748017*RA*RA-0.004185405*RA-3.65278283);
+				//S2
+				//daydata->records[r].stokes.I -= (0.0000330741985*RA*RA-0.0116655548*RA-2.41301002);
+				//N4
+				//daydata->records[r].stokes.I -= (0.0000000957942322*RA*RA+0.00644514212*RA-5.84658068);
+				daydata->records[r].stokes.I -= (c[2]*RA*RA+c[1]*RA-c[0]);
 			}
-		}
-		if( chan == 0 ) {
-			int k = 0;
-			for(k = md->avg_lowchan;k< md->avg_highchan;k++)
-			{
-				for(b=0;b<7;b++)
-				{
-					spec_I[b][0]+=spec_I[b][k];
-				}
-			}
-			for(b=0;b<7;b++)
-			{
-				spec_I[b][0]/=(md->avg_highchan - md->avg_lowchan);
-			}
-		}
-
-	}
-*/
-
-
-	FluxDayData * daydata = &wappdata->daydata[day];
-	int R = daydata->numRecords;
-	int r;
-	for (r = 0; r < R; r++)
-	{
-		if (isfinite(daydata->records[r].stokes.I))
-		{
-			float RA = daydata->records[r].RA;
-			//S1
-			//daydata->records[r].stokes.I -= (0.0000479748017*RA*RA-0.004185405*RA-3.65278283);
-			//S2
-			//daydata->records[r].stokes.I -= (0.0000330741985*RA*RA-0.0116655548*RA-2.41301002);
-			//N4
-			daydata->records[r].stokes.I -= (0.0000000957942322*RA*RA+0.00644514212*RA-5.84658068);
 		}
 	}
 	return;

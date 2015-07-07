753f9178d4f4af1494c8cb4a1eec7e1485885dae
Kevin Thiart <kevinthiart@gmail.com>
2015-07-07 15:59:23 +0200

    fixed implicit declaration of function warnings for write_rfi_data

--- cal/stokes.h
+++ cal/stokes.h
@@ -11,5 +11,6 @@ void average_stokes(SpecRecord dataset[], int size, int lowchan, int highchan, f
 void correct_beamgains(SpecRecord dataset[], int size, int lowchan, int highchan, int beam);
 void write_binary_channel_data_single_file(SpecRecord dataset[], int numRecords, int lowchan, int highchan);
 void write_binary_average_data(SpecRecord dataset[], int size, int lowchan, int highchan);
+void write_rfi_data( SpecRecord dataset[], int numRecords, int lowchan, int highchan);
 #endif //_STOKES_H
 
